0b34302e36eebaf6ec28cf944b0f20ed70c0098a
nahsooyeon <nahsooyeon@gmail.com>
2022-07-10 22:34:43 +0900

    🔨 수정: 세션 연결

--- server/index.ts
+++ server/index.ts
@@ -2,13 +2,41 @@ import next from "next";
 import Hapi from "@hapi/hapi";
 import { nextHandlerWrapper } from "./next-wrapper";
 import { helloRoutes, sendJson } from "./routes/hello";
+import Hoek from "@hapi/hoek";
+
+import CatboxRedis from "@hapi/catbox-redis";
 
 const port = parseInt(process.env.PORT as string, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const server = new Hapi.Server({
   port,
+  cache: [
+    {
+      name: "my_cache",
+      provider: {
+        constructor: CatboxRedis,
+        options: {
+          partition: "my_cached_data",
+          host: "127.0.0.1",
+          port: 6379,
+          db: 0,
+        },
+      },
+    },
+  ],
 });
+
+const sessionOptions = {
+  maxCookieSize: 0, // force server-side storage
+  cache: {
+    cache: "my_cache",
+  },
+  cookieOptions: {
+    password: "abcdefghijklmnodfjlskafjlsdfjlslsfjldsf", // cookie password
+    isSecure: false, // allow non HTTPS
+  },
+};
 
 app.prepare().then(async () => {
   server.route({
@@ -40,6 +68,10 @@ app.prepare().then(async () => {
   });
 
   try {
+    await server.register({
+      plugin: require("@hapi/yar"),
+      options: sessionOptions,
+    });
     await server.start();
     console.log(`> Ready on http://localhost:${port}`);
   } catch (error) {
